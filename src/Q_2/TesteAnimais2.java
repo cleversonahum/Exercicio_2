5ef1aaeb1525d4d4bb7130bb4c7e502d57d76ead
Cleverson Nahum <cleversonahum@gmail.com>
2016-01-28 17:59:06 -0300

    Trabalhando em testeAnimais2

---
+++ src/Q_2/TesteAnimais2.java
@@ -0,0 +1,15 @@
+package Q_2;
+//import javax.swing.JOptionPane;
+
+public class TesteAnimais2 {
+
+	public static void main(String[] args) {
+		Animal animais[] = new Animal[2];
+		//Peixe[] tubarao = new Peixe();
+		//Mamifero[] urso_do_canada = new Mamifero();
+		animais[0].alteraNome("Cleverson");
+		animais[1].alteraNome("outro cleverson");
+
+	}
+
+}
